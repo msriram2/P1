29b7cd6055a4d935aea57b1cbb9ea6e244ba939f
Meera Sriram <msriram2@uci.edu>
2024-07-26 00:09:42 -0700

    Completed and edited part 7 with comments for extra credit

--- ICS33-Project1/part7.py
+++ ICS33-Project1/part7.py
@@ -1,2 +1,12 @@
 #Write your commend using python comments
 #Answer:
+
+"""
+When wanting to create a local repository connected to my remote repository, the following steps should be followed:
+1) Initialize the directory with files you want to edit and commit.
+2) Use git clone to include the URL or SSL to the remote repository.
+3) Use git remote add to push your first commit to the remote repository. Can also use commit to make commits to local
+repository
+4) For future commits, git push is all that is needed.
+"""
+
